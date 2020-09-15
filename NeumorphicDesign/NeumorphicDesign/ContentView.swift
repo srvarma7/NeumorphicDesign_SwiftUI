9a23d77b972a3c7ce95eecfb1118fbedd07df654
srvarma7 <srvarma7@gmail.com>
2020-09-15 15:02:08 +1000

    Adding project files

---
+++ NeumorphicDesign/NeumorphicDesign/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  NeumorphicDesign
+//
+//  Created by Sai Raghu Varma Kallepalli on 15/9/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
